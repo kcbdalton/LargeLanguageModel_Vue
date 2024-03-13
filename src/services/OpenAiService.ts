40bc360b3a0f8cba1723ef502fc7a260c671f885
BenjiDalton <benjamin.dalton@outlook.com>
2024-03-13 16:10:06 -0400

    Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/services/OpenAiService.ts

--- src/services/OpenAiService.ts
+++ src/services/OpenAiService.ts
@@ -2,16 +2,21 @@
 
 import OpenAI from "openai";
 import { environment } from "environment";
+import { ChatCompletionContentPart } from "openai/resources";
 
-const openai = new OpenAI({apiKey: environment.OPENAT_API_KEY});
+const openAI = new OpenAI({apiKey: environment.OPENAT_API_KEY});
+const modelEngine = 'gpt-3.5-turbo';
 
-async function main() {
-	const completion = await openai.chat.completions.create({
-		messages: [{ role: "system", content: "You are a helpful assistant." }],
-		model: "gpt-3.5-turbo",
-	});
-
-	console.log(completion.choices[0]);
-}
-
-main()
+export default {
+	async makeOpenAIRequest(prompt: string | ChatCompletionContentPart[]): Promise<string> {
+		const response = await openAI.chat.completions.create({
+			model: modelEngine,
+			messages: [{
+				role: 'user',
+				content: prompt
+			}],
+			stream: false
+		});
+		return response.choices[0].message.content;
+	}
+};
