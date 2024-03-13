ef1740c8381794d78e2a34278a7ec22b1def6d2b
BenjiDalton <benjamin.dalton@outlook.com>
2024-03-13 15:52:55 -0400

    Changes to be committed: 	new file:   environment.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   src/services/OpenAiService.ts 	new file:   src/services/demo.txt

---
+++ environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    OPENAT_API_KEY: '',
+}
--- src/services/OpenAiService.ts
+++ src/services/OpenAiService.ts
@@ -1,1 +1,17 @@
 'https://platform.openai.com/docs/assistants/tools/code-interpreter?lang=node.js'
+
+import OpenAI from "openai";
+import { environment } from "environment";
+
+const openai = new OpenAI({apiKey: environment.OPENAT_API_KEY});
+
+async function main() {
+	const completion = await openai.chat.completions.create({
+		messages: [{ role: "system", content: "You are a helpful assistant." }],
+		model: "gpt-3.5-turbo",
+	});
+
+	console.log(completion.choices[0]);
+}
+
+main()
